18819d6792193a711e650ac0b7aa91d7e6c22e7d
rajavasanthan <Rajavasanthan@gmail.com>
2020-05-05 20:34:12 +0530

    Changes for Production

--- src/app/config/configurations.ts
+++ src/app/config/configurations.ts
@@ -1,8 +1,7 @@
 export class Configurations {
-
-    public static SERVER_PATH = 'http://localhost/ng/gised/src/app/server/server.php';
-    public static SERVER_PATH_UPLOAD = 'http://localhost/ng/gised/src/app/server/classesmodule/class.module.uploadFiles.php';
-    public static SERVER_PATH_DOWNLOAD = 'http://localhost/ng/gised/src/app/server/classesmodule/class.module.downloadFiles.php?fileName=';
+    public static SERVER_PATH = 'http://gised.cyferx.com/backend/server.php';
+    public static SERVER_PATH_UPLOAD = 'http://gised.cyferx.com/backend/classesmodule/class.module.uploadFiles.php';
+    public static SERVER_PATH_DOWNLOAD = 'http://gised.cyferx.com/backend/classesmodule/class.module.downloadFiles.php?fileName=';
     public static MAX_FILE_UPLOAD_SIZE  = 15 * 1024 *1024;
     public static FILE_UPLOAD_ACCEPET_TYPES  = 'image/*,.pdf,.doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document';
 
